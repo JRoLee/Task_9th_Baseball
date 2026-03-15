d078d631471e1dd003ef117f02aad2087bf289a8
JRoLee <cnsfw123@gmail.com>
2026-03-15 21:10:55 +0900

    Add : GameState BP and class
    Add : GameState BP and class

---
+++ Source/Task_9th_Baseball/Public/Game/NBGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "NBGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TASK_9TH_BASEBALL_API ANBGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
