a446041774d64a8c1d572763c4731ed70913399f
JRoLee <cnsfw123@gmail.com>
2026-03-16 20:07:10 +0900

    Add : PlayerState class and BP
    Add : PlayerState class and BP

---
+++ Source/Task_9th_Baseball/Public/Player/NBPlayerState.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "NBPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TASK_9TH_BASEBALL_API ANBPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	ANBPlayerState();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(Replicated)
+	FString PlayerNameString;
+	
+	UPROPERTY(Replicated)
+	int32 CurrentGuessCount;
+	
+	UPROPERTY(Replicated, EditAnywhere)
+	int32 MaxGuessCount;
+	
+public:
+	FString GetPlayerInfoString();
+};
